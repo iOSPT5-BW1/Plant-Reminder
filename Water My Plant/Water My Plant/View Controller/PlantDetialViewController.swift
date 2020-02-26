32ad2efd89860c25bada38b2577173a42597ed99
Breena <breena_greek@live.com>
2020-02-26 00:03:56 -0500

    Initial Commit

---
+++ Water My Plant/Water My Plant/View Controller/PlantDetialViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PlantDetialViewController.swift
+//  Water My Plant
+//
+//  Created by Breena Greek on 2/25/20.
+//  Copyright © 2020 Breena Greek. All rights reserved.
+//
+
+import UIKit
+
+class PlantDetialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
